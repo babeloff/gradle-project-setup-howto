df1771b8f978be64220be9bfa18f2c0703c1a92b
fred eisele <fred.eisele@vanderbilt.edu>
2022-06-08 12:11:57 -0500

    align list with script

--- edits/13_rename_files.sh
+++
@@ -1,4 +0,0 @@
-
-git ls-files | grep 'org\.example' | sed 's/org\.example/  /' | sort -u > ./edits/13_org_example_files.list
-
-while read pre post ; do git mv ${pre}org.example${post}  ${pre}edu.vanderbilt.isis.mesolab${post} ; done < ./edits/13_org_example_files.list
