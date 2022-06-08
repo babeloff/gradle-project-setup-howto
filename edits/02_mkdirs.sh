3f8cc1e72eba6a54d05b9debf3353cb637f70c5b
fred eisele <fred.eisele@vanderbilt.edu>
2022-06-08 10:59:11 -0500

    rename group name in git directories

--- edits/02_mkdirs.sh
+++ edits/02_mkdirs.sh
@@ -1,10 +1,9 @@
 
-find . -path '*/org/example' | grep -v 'build' | sed 's|org/example||g' > ./edits/org_example_dirs.list
+git ls-files --directory | while read file; do echo ./$(dirname ${file}) ; done | sort -u | grep '\/org\/example\/' | sed 's|org/example/.*||g' > ./edits/org_example_dirs.list0
 
-while read it ; do mkdir -p ${it}edu/vanderbilt/isis/mesolab ; done < ./edits/org_example_dirs.list
-while read it ; do mv ${it}org/example/* ${it}edu/vanderbilt/isis/mesolab/ ; done < ./edits/org_example_dirs.list 
-while read it ; do rm -rf ${it}org ; done < ./edits/org_example_dirs.list 
+mv ./edits/org_example_dirs.list0 ./edits/org_example_dirs.list
 
-while read it ; do git add ${it}edu ; done < ./edits/org_example_dirs.list 
+while read it ; do rmdir ${it}edu/vanderbilt/isis/mesolab ; done < ./edits/org_example_dirs.list
+while read it ; do git mv ${it}org/example ${it}edu/vanderbilt/isis/mesolab ; done < ./edits/org_example_dirs.list
 
 
